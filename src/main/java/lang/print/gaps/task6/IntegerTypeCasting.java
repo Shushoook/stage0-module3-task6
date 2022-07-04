69c6bbb248fda1adbfcaf4da8476669e7694e3d2
Shushoook <shushoook666@gmail.com>
2022-07-04 20:50:50 +0400

    stage0-module3-task6

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        long firstName = 1000l;
+        int secondName = (int) firstName;
+
+        System.out.println(firstName);
+        System.out.println(secondName);
+
     }
 }
